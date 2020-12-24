a3c7f5db85a43349cd0e577ec494605844eb687e
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-24 01:38:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do 
-    count += 1 
     if array[count] == value_to_find
       return count
     end 
+    count += 1 
   end
 end
